6f4b38c82902fd99348d36fb0d743e092aa994a0
Finn Harms <github@finn-harms.de>
2023-06-09 17:12:01 +0200

    refactoring

---
+++ scheinicam/recording_controller.py
@@ -0,0 +1,55 @@
+from obscontroller import ObsController
+from settings import Settings
+from filemanager import Filemanager
+
+import logging
+import datetime
+
+class RecordingController:
+    def __init__(self, obs_controller: ObsController, settings: Settings, filemanager: Filemanager):
+        self.obs_controller = obs_controller
+        self.settings = settings
+        self.filemanager = filemanager
+
+    def auto_record(self):
+        if not self.obs_controller.connected:
+            return
+        now = datetime.datetime.now().time()
+        if now > self.settings.start_time and now < self.settings.end_time:
+            if not self.obs_controller.recording:
+                logging.info("Starting recording")
+                self.start_record()
+        else:
+            if self.obs_controller.recording:
+                logging.info("Stopping recording")
+                self.stop_record()
+
+    def start_record(self):
+        if not self.obs_controller.connected or self.obs_controller.recording:
+            logging.warning("Could not start recording because OBS is not connected or already recording")
+            return
+        try:
+            file = self.obs_controller.record()
+            if file is None:
+                logging.error("Could not start recording because file is None")
+                return
+            self.filemanager.add_file(file)
+            file.export_as_json()
+            logging.info(f"Started recording {file.filename}")
+        except Exception as e:
+            logging.exception(e)
+            logging.error(f"Could not start recording")
+            return
+
+    def stop_record(self):
+        if not self.obs_controller.connected or not self.obs_controller.recording:
+            logging.warning("Could not stop recording because OBS is not connected or not recording")
+            return
+        try:
+            self.obs_controller.stop()
+            self.obs_controller.file.stop_recording()
+            logging.info(f"Stopped recording {self.obs_controller.file.filename}")
+        except Exception as e:
+            logging.exception(e)
+            logging.error(f"Could not stop recording")
+            return
---
+++ scheinicam/settings.py
@@ -0,0 +1,44 @@
+import datetime
+import json
+import logging
+
+SETTINGS_FILE = "settings.json"
+
+class Settings:
+    def __init__(self):
+        '''Load settings from json or set default values'''
+        if not self.load_from_json(SETTINGS_FILE):
+            self.set_default_values()
+            self.save_to_json(SETTINGS_FILE)
+
+    def load_from_json(self, filename: str) -> bool:
+        '''Load settings from json, return True if successful'''
+        try:
+            data = json.load(open(filename, "r"))
+            self.start_time = datetime.fromisoformat(data["start_time"])
+            self.end_time = datetime.fromisoformat(data["end_time"])
+            self.delete_age = datetime.timedelta(seconds=data["delete_age"])
+            return True
+        except Exception as e:
+            logging.exception(e)
+            logging.warning(f"Could not load settings from {filename}")
+            return False
+        
+    def save_to_json(self, filename: str):
+        '''Save settings to json'''
+        try:
+            data = {
+                "start_time": self.start_time.isoformat(),
+                "end_time": self.end_time.isoformat(),
+                "delete_age": self.delete_age.total_seconds()
+            }
+            json.dump(data, open(filename, "w"))
+        except Exception as e:
+            logging.exception(e)
+            logging.error(f"Could not save settings to {filename}")
+
+    def set_default_values(self):
+        '''Set default values'''
+        self.start_time = datetime.time(19, 55, 0)
+        self.end_time = datetime.time(22, 55, 0)
+        self.delete_age = datetime.timedelta(days=7)
