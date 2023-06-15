b31b7e4b70c68f3683f08ea76446682a81ae281e
Finn Harms <github@finn-harms.de>
2023-06-15 17:57:48 +0200

    Admin-Page layout update

--- scheinicam/pages/admin.py
+++ scheinicam/pages/admin.py
@@ -8,8 +8,8 @@ from obscontroller import ObsController
 def admin_page(obs_controller: ObsController):
     # Add Button to Mute and Unmute Recording
     with ui.card().bind_visibility_from(obs_controller, 'connected'), ui.row().style("margin-top: 1em;"):
-        ui.button('Aufnahme stumm schalten', color="red", on_click=obs_controller.mute_video)
-        ui.button('Aufnahme wieder einschalten', color="blue", on_click=obs_controller.unmute_video)
-    with ui.card():
+        ui.button('Aufnahme stumm schalten', color="red", on_click=obs_controller.mute_video).classes("w-full").style("height: 6em;")
+        ui.button('Aufnahme wieder einschalten', color="blue", on_click=obs_controller.unmute_video).classes("w-full").style("height: 6em;")
+    with ui.expansion("Erweiterte Funktionen"):
         ui.button("Computer herunterfahren", color="red", on_click=lambda: os.system("shutdown /s /t 1"))
     # TODO Add options for start and end time
