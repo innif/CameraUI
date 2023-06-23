7875bc0a52e18c1484325cc07a914285df93d992
Finn Harms <github@finn-harms.de>
2023-06-23 17:15:32 +0200

    Reload Camera admin page

--- scheinicam/pages/admin.py
+++ scheinicam/pages/admin.py
@@ -25,7 +25,8 @@ def admin_page(obs_controller: ObsController, filemanager: Filemanager):
             ui.button('Aufnahme stumm schalten', color="red", on_click=obs_controller.mute_video).classes("w-full").style("height: 6em;")
             ui.button('Aufnahme wieder einschalten', color="blue", on_click=obs_controller.unmute_video).classes("w-full").style("height: 6em;")
         with ui.expansion("Erweiterte Funktionen").classes("w-full").style("max-width: 600px;"):
-            ui.button("Computer herunterfahren", color="red", on_click=lambda: os.system("shutdown /s /t 1")).classes("w-full")
+            ui.button("Kamera neu laden", color="blue", on_click=obs_controller.reload_camera).classes("w-full").style("margin-bottom: 1em;")
+            ui.button("Computer herunterfahren", color="red", on_click=lambda: os.system("shutdown /s /t 1")).classes("w-full").style("margin-bottom: 1em;")
         with ui.expansion("Logfiles").classes("w-full").style("max-width: 600px;"):
             with ui.column():
                 for file in os.listdir("logs"):
